44d38428516920583f20402e17e7431c70d699fe
virtualzeth <virtualzeth@gmail.com>
2020-09-18 11:50:57 +0200

    bug fixes

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -34,29 +34,30 @@ public class MiniComputer {
 
 
         }
-        public static void RNGguess(){
+    }
+    public static void RNGguess() {
 
-             double RNG;
+        double RNG;
 
-             // importer java.util.random
-             Random rand = new Random();
+        // importer java.util.random
+        Random rand = new Random();
 
-             //siger hvad nummer den skal være imellem i forhold til et til 20
-             int Numbers = rand.nextInt(10);
+        //siger hvad nummer den skal være imellem i forhold til et til 20
+        int Numbers = rand.nextInt(10);
 
-             //fortæller tallet der skal gættes imellem. der kan gættes udenfor 1-20, men der er et rigtigt tal mellem 1-20.
-             System.out.println("Guess the number from 1 to 10");
+        //fortæller tallet der skal gættes imellem. der kan gættes udenfor 1-20, men der er et rigtigt tal mellem 1-20.
+        System.out.println("Guess the number from 1 to 10");
 
-             // tjekker for tallet.
-             RNG = sc.nextDouble();
+        // tjekker for tallet.
+        RNG = sc.nextDouble();
 
-             //ser om tallet er rigtigt eller forkert, og gir svar til givende tal.
-             if (Numbers == RNG) {
-                System.out.println("You got it right!");
-             } else {
-                System.out.println("Better luck another time!");
-             }
+        //ser om tallet er rigtigt eller forkert, og gir svar til givende tal.
+        if (Numbers == RNG) {
+            System.out.println("You got it right!");
+        } else {
+            System.out.println("Better luck another time!");
         }
+    }
     public static void getTime() {
         System.out.println(LocalDateTime.now());
     }
