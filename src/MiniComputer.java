6d54bad6b59e6058872cd665f5808ebbead6916c
Gustav <70881859+gusajer@users.noreply.github.com>
2020-09-18 11:26:09 +0200

    her er mit udkast

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -8,7 +8,7 @@ public class MiniComputer {
         boolean gate = true;
         while(gate) {
             System.out.println("[programnavn]");
-            System.out.println("[programnavn]");
+            System.out.println("[Konverter sekunder til timer]");
             System.out.println("[programnavn]");
             System.out.println("[programnavn]");
             System.out.println("[programnavn]");
@@ -25,5 +25,16 @@ public class MiniComputer {
                 if(input.equals("0")) gate = false;
             } else System.out.println("Input error!");
         }
+    }
+    public static void Konverter() {
+        Scanner input = new Scanner(System.in);
+        int Mimuter = input.nextInt();
+
+        System.out.print("Skriv et intenger for Mimuter : ");
+
+        int Timer = Mimuter / 60;
+        int ResterendeSekunder = Mimuter % 60;
+        System.out.println(Mimuter + " Mimuter er " + Timer +
+                " Timer og " + ResterendeSekunder + " Mimuter");
     }
 }
