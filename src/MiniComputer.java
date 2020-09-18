5f43c30ac638a7db26aae5258378ae56b3eab9f2
virtualzeth <virtualzeth@gmail.com>
2020-09-18 12:24:17 +0200

    Bugfixes & final touches

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -8,24 +8,20 @@ public class MiniComputer {
     public static void main(String[] args) {
         boolean gate = true;
         while (gate) {
-            System.out.println("[programnavn]");
+            System.out.println("1 - GetLottoTal");
             System.out.println("2 - Konverter sekunder til timer");
-            System.out.println("[programnavn]");
-            System.out.println("4 - getTime");
-            System.out.println("5 - RNGguess");
-            System.out.println("[programnavn]");
-            System.out.println("[programnavn]");
-            System.out.println("[programnavn]");
-            System.out.println("[programnavn]");
+            System.out.println("3 - getTime");
+            System.out.println("4 - RNGguess");
             System.out.println("0 - Exit");
           
             System.out.println("What program do you want to run?");
             if (sc.hasNextLine()) {
                 String input = sc.nextLine();
                 if(input.equals("0")) gate = false;
+                if(input.equals("1")) GetLottoTal();
                 if(input.equals("2")) converter();
-                if(input.equals("4")) getTime();
-                if(input.equals("5")) RNGguess();
+                if(input.equals("3")) getTime();
+                if(input.equals("4")) RNGguess();
                 System.out.println("Press enter when ready to continue...");
                 sc.nextLine();
             } else System.out.println("Input error!");
@@ -33,25 +29,24 @@ public class MiniComputer {
     }
     public static void RNGguess() {
 
-        double RNG;
-
         // importer java.util.random
         Random rand = new Random();
 
-        //siger hvad nummer den skal være imellem i forhold til et til 20
+        //siger hvad nummer den skal være imellem i forhold til et til 10
         int Numbers = rand.nextInt(10);
 
         //fortæller tallet der skal gættes imellem. der kan gættes udenfor 1-20, men der er et rigtigt tal mellem 1-20.
         System.out.println("Guess the number from 1 to 10");
 
         // tjekker for tallet.
-        RNG = sc.nextDouble();
+        String input = sc.nextLine();
+        double RNG = Double.parseDouble(input);
 
         //ser om tallet er rigtigt eller forkert, og gir svar til givende tal.
         if (Numbers == RNG) {
             System.out.println("You got it right!");
         } else {
-            System.out.println("Better luck another time!");
+            System.out.println("Better luck next time!");
         }
     }
     public static void getTime() {
