43211f6d72c814e6ba8411d2a587856e7faf9066
Chilas Main PC <Chil0041@edu.easj.dk>
2020-09-18 11:26:00 +0200

    Lotto generator

--- src/MiniComputer.java
+++ src/MiniComputer.java
@@ -26,4 +26,12 @@ public class MiniComputer {
             } else System.out.println("Input error!");
         }
     }
+    public static void GetLottoTal() {
+        int[] tal =  new int[7];
+        for (int i = 0; i < 6; i++) {
+            int a = (int)(Math.random() * 101);
+            tal[i] =  a ;
+        }
+        System.out.println( "Lotta tallene er " + tal[0] + " + " + tal[1] + " +  "+ tal[2] +" + "+ tal[3] + " + "+ tal[4] + " + "+ tal[5] + " + "+ tal[6]);
+    }
 }
